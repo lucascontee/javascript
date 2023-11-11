8285cf4b65869d6e18fb75617ca786ebeafa629b
Lucas Conte <144933020+lucascontee@users.noreply.github.com>
2023-11-10 23:22:01 -0300

    finalizando exercicios while

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,25 +1,20 @@
 function clicar(){
     var num = document.getElementById('numero')
     var tab = document.getElementById('tab')
-    var res = document.getElementById('res')
     
 
     if(num.value.length == 0){
-        window.alert('Digite um número')
+        window.alert('DIGITE UM NÚMERO')
     } else{
         var n = Number(num.value)
         var c = 1
         tab.innerHTML = ''
         
-        for(var c ; c <=10; c++){
+
+        for(var c; c <=10; c++){
             var item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
+            item.text = `${n} X ${c} = ${n*c}`
             tab.appendChild(item)
         }
     }
-
-
-    
-    
-    
 }
